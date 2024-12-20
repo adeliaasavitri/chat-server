f821c124954833024ecf31496df090418e29a28a
adeliaasavitri <adeliaasavitri@gmail.com>
2024-12-20 13:32:16 +0800

    alter install.js

--- install.js
+++ install.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-// Read the dependencies from the text file
-const data = fs.readFileSync('dependencies.txt', 'utf8');
-const dependencies = JSON.parse(data).dependencies;
+// Read the package.json file
+const packageJson = fs.readFileSync('package.json');
+const packageData = JSON.parse(packageJson);
+
+// Extract dependencies from package.json
+const dependencies = packageData.dependencies;
 
 // Install each dependency
 for (const [dependency, version] of Object.entries(dependencies)) {
     console.log(`Installing ${dependency}@${version}...`);
     execSync(`npm install ${dependency}@${version}`);
 }
+
+console.log('All dependencies installed successfully.');
